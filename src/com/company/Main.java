257e464f02f811f849d91a9076b11fa26d33085f
mattrorvick <matthewrorvick@gmail.com>
2021-02-01 21:54:56 -0600

    init commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+//        double calculation = CalcFtAndInToCm.calcFeetAndInchesToCentimeters(6, 13);
+//        System.out.println(calculation);
+        CalcFtAndInToCm.calcFeetAndInchesToCentimeters(100);
+
+    }
+}
